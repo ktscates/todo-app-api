a8556f35da687304f2ff79e3d7f097fb9a4dc887
ktscates <cates2506@gmail.com>
2023-11-24 16:34:33 +0200

    enable cors on all routes

--- api/todos/index.js
+++ api/todos/index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Enable CORS for all routes
+app.use(cors());
+
 app.use(express.json());
 
 let todos = [];
